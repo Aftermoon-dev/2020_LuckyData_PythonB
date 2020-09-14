3f05831bc865630f53f393dd48fceaaa1806479f
Minjae Seon <ddol0225@gmail.com>
2020-09-14 22:43:03 +0900

    Change decimaltobinary.py

--- 1wk/Function in Major/decimaltobinary.py
+++ 1wk/Function in Major/decimaltobinary.py
@@ -1,21 +1,26 @@
+# 10진수 숫자를 2진수로 변환하기
+
+def decimaltobinary(dec):
+    binarylist = []
+    while True:
+        # if number is 1 or 0, break 
+        if dec == 1 or dec == 0:
+            binarylist.append(dec)
+            break
+        else:
+            binarylist.append(dec % 2)
+            dec = (int) (dec / 2)
+
+    # Print List Element (Reverse)
+    print("Binary Number : ", end='')
+    for i in range(len(binarylist)-1, -1, -1):
+        print(binarylist[i], end='')
+
+
 print("Decimal to Binary")
-dec = int(input("Please Enter Decimal Number : "))
-deccopy = dec
-binarylist = []
+num = int(input("Please Enter Decimal Number : "))
+decimaltobinary(num)
 
-while True:
-    # if number is 1 or 0, break 
-    if dec == 1 or dec == 0:
-        binarylist.append(dec)
-        break
-    else:
-        binarylist.append(dec % 2)
-        dec = (int) (dec / 2)
-
-print("Binary Number : ", end='')
-for i in range(len(binarylist)-1, -1, -1):
-    print(binarylist[i], end='')
-
+# 파이썬 내장 함수와 비교
 print("\nPython bin() function : ", end='')
-print(bin(deccopy))
-
+print(bin(num))
